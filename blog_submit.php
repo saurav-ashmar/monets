032f90490b68b5acec8cc4b187dabc762e7cf3e4
saurav-ashmar <saurav.kumar@ashmar.in>
2015-11-26 16:20:31 +0530

    Create Blog_submit.php

---
+++ blog_submit.php
@@ -0,0 +1,11 @@
+<form name="myform" id="myform" method="post" action="add_comment.php">
+    <h2>Please leave a comment:</h2>
+    <input type="text" id="username" name="username" value="Enter Username" /><br />
+    <input type="text" id="email" name="email" value="Enter Email" /><br />
+    <textarea id="comment" name="comment">Enter Comment</textarea><br />
+    <br /><br />
+    <input type="checkbox" id="notify_box" name="notify_box" value="Y">
+    Notify me when a new post is published. <br />
+    <br />
+    <input type="submit" value="Post Comment">
+</form>
